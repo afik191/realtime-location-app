4f2b7381270de620246be160b23beefba8319538
Afik Aharon <afik3248@gmail.com>
2025-07-07 11:52:08 +0300

    a

--- server/index.js
+++ server/index.js
@@ -307,7 +307,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 
-const clientBuildPath = path.join(__dirname, "client-build");
+const clientBuildPath = path.join(__dirname, "client", "dist"); 
+
 
 app.use(express.static(clientBuildPath));
 
